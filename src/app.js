74e41087b5f0ef5ba80c1468ef544e1f2cc69053
Hugoleosilva <hugollsilva.dev@gmail.com>
2026-04-10 16:06:02 -0300

    ADD o Swagger como página principal

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 import fs from 'fs';
 import swaggerUi from 'swagger-ui-express';
 import movieRoutes from './routes/movieRoutes.js';
 
-// Lendo o arquivo JSON do Swagger
 const swaggerFile = JSON.parse(fs.readFileSync('./swagger.json', 'utf8'));
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-// Documentação
+// 1. Rota principal redirecionando para o Swagger
+app.get('/', (req, res) => {
+    res.redirect('/api-docs');
+});
+
+// 2. Configuração do Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// Rotas
+// 3. Rotas da API
 app.use(movieRoutes);
 
 export default app;
